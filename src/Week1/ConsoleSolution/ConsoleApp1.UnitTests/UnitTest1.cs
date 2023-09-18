5121ac29fecd21303bd0522191ef4c019480dce7
Kelsey Bristol <kelsey_bristol@progressive.com>
2023-09-18 16:26:06 -0400

    End of day 1

---
+++ src/Week1/ConsoleSolution/ConsoleApp1.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ConsoleApp1.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/Week1/ConsoleSolution/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("meowwwwwwww");
+
+string message = "I love Argy";
+
+Console.WriteLine($"The really important msg I have to say is: {message}");
+
+var app = builder.Build();
+
+string finalMessage = $"The Message {message}";
+
+app.MapGet("/message", () =>
+{
+    return Results.Ok(finalMessage);
+});
+
+app.Run();
